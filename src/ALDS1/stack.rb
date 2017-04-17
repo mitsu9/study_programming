a13bf45e16d99a7a4f9c22e91941a36800e5575a
mitsu9 <mitsunobu.h.9@gmail.com>
2017-04-17 17:31:52 +0900

    Add comment

--- src/ALDS1/stack.rb
+++ src/ALDS1/stack.rb
@@ -2,13 +2,35 @@
 stack = []
 gets.split(' ').each { |op|
   case op
-  when /^[0-9]+$/; stack.push v.to_i
-  when /^\+$/; stack.push(stack.pop + stack.pop)
+  when /^[0-9]+$/; stack << op.to_i
+  when /^\+$/; stack << stack.pop + stack.pop
   when /^-$/;
     b = stack.pop
-    stack.push(stack.pop - b)
-  when /^*$/; stack.push(stack.pop * stack.pop)
+    stack << stack.pop - b
+  when /^*$/; stack << stack.pop * stack.pop
   else puts "Error : not match"
   end
 }
 puts stack.pop
+
+=begin
+
+# pop(2)とinjectを使って書き換えた場合
+case op
+when /^[0-9]+$/; stack << op.to_i
+when /^\+$/; stack << stack.pop(2).inject(:+)
+when /^-$/; stack << stack.pop(2).inject(:-)
+when /^*$/; stack << stack.pop(2).inject(:*)
+else puts "Error : not match"
+end
+
+# 上のものをさらにまとめる
+# $&は正規表現にマッチした部分を持った変数
+# 正規表現の中は+を先に書くと意味がおかしくなってエラーになる
+case op
+when /^[0-9]+$/; stack << op.to_i
+when /^[-+*]$/; stack << stack.pop(2).inject($&)
+else puts "Error : not match"
+end
+
+=end
